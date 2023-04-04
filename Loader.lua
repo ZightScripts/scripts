6da5b7b7cbc804c2b717b090f0c66ea6d707aa3d
ZightScripts <129881536+ZightScripts@users.noreply.github.com>
2023-04-05 02:14:29 +1000

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,16 @@
 task.wait(20) -- Taskwait to ensure it auto-executes.
 
+-- GLOBAL ANTIAFK
+loadstring(game:HttpGet('https://pastebin.com/raw/XniN0RP9'))()
+
 local games = {
+    -- Clicker Simulator: Custom
     [{7560156054}] = "https://raw.githubusercontent.com/ZightScripts/scripts/main/ClickerSim.lua",
-    [{8982709021}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/YouTube%20Life/Auto%20Farm.lua",
-    [{537413528}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Build%20A%20Boat%20For%20Treasure/Script.lua",
+    -- Bedwars: VapeV4
+    [{6872265039}] = "https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua",
+    -- Arsenal: Bolts Hub
+    [{286090429}] = "https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main",
+    --
     [{3101667897}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Legends%20Of%20Speed/Script.lua",
     [{3956818381}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Ninja%20Legends/Script.lua",
     [{4623386862, 5661005779}] = "https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Piggy/Loader.lua",
